6f148f7e934f213b544e911944e4d60c2dc7ef0d
DEBASHIS NAYAK <cdnayak2000@gmail.com>
2024-08-20 20:21:12 +0530

    hero section text updated

--- qtify/src/components/Hero/Herosection.jsx
+++ qtify/src/components/Hero/Herosection.jsx
@@ -6,7 +6,7 @@ const Herosection = () => {
   return (
     <Box className={styles.heroContainer}>
       <Box className={styles.textHolder}>
-        <Box className={styles.textContainer}>100 Thousands Songs,ad-free</Box>
+        <Box className={styles.textContainer}>100 Thousand Songs, ad-free</Box>
         <Box className={styles.textContainer}>Over thousands podcast episodes</Box>
       </Box>
       <Box className={styles.imageContainer}></Box>
